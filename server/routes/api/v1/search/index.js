3f0926f7eea88615f9797c0121f596419b230b9c
sudipto <sudiptocsi@gmail.com>
2022-02-17 19:15:04 +0600

    some minor clean

--- server/routes/api/v1/search/index.js
+++ server/routes/api/v1/search/index.js
@@ -10,20 +10,13 @@ module.exports = (app) => {
     try {
       axios.get('https://itunes.apple.com/search?term=nirvana')
         .then(function (response) {
-          // handle success
-          // console.log(response.data.results);
           const getResultData = response.data.results.length;
           res.status(200).send(responseObj.responseFormat("routes found here", true, response.data.results, getResultData));
         })
         .catch(function (error) {
-          // handle error
-          //console.log(error);
           res.status(417).send(responseObj.responseFormat("exception handled", true, {
             error: error.message
           }, 0));
-        })
-        .then(function () {
-          // always executed
         });
     } catch (err) {
       res.status(417).send(responseObj.responseFormat("exception handled", true, {
